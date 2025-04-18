314fd2fbb0c8959e53a6ee334da5f0c337a0ee5e
Braian Torres <brianntorress.s@gmail.com>
2025-04-17 23:35:29 -0300

    mini database en memoria creada

---
+++ app/database/fake_database.py
@@ -0,0 +1,7 @@
+from models.user_model import User
+
+users = [
+    User(id=0, name="User 1", last_name="last name 1", age=50),
+    User(id=1, name="User 2", last_name="last name 2", age=25),
+    User(id=2, name="User 3", last_name="last name 3", age=15)
+]
