5df51e2fff9a4c82d1ee1704890f884e24ef436f
Braian Torres <brianntorress.s@gmail.com>
2025-04-19 16:33:30 -0300

    campos 'api key' y 'rol' añadidos al 'user model'

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
+from app.constants.roles import Rol
 
 class User(BaseModel):
     id: int
     name: str
     last_name: str
     age: int
+    rol: Rol
+    api_key: str
