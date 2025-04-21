d633a2918ad032c2f3bbab8ef23262d9bb3f24c6
Braian Torres <brianntorress.s@gmail.com>
2025-04-21 03:33:12 -0300

    content routes added to main app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.exceptions import RequestValidationError
 from app.routes.user_routes import router as user_router
+from app.routes.content_routes import router as content_router
 from app.exceptions.handlers import  http_exception_handler, validation_exception_handler, general_exception_handler
 
 app = FastAPI()
@@ -10,3 +11,4 @@ app.add_exception_handler(RequestValidationError, validation_exception_handler)
 app.add_exception_handler(Exception, general_exception_handler)
 
 app.include_router(user_router, prefix="/users")
+app.include_router(content_router, prefix="/contents")
